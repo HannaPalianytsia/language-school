9717d392d469093aaa4bbc2192fe99cc80153c37
HannaPalianytsia <hannapalianytsia1@gmail.com>
2023-10-18 11:36:36 +0300

    add js

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+// document.getElementById('get-started-btn').onclick = document.getElementById
+
+// Вибираємо кнопку та текстовий елемент за ідентифікаторами
+// var changeTextButton = document.getElementById("get-started-btn");
+// var textElement = document.getElementById("main-text");
+
+// Додаємо обробник події для кнопки
+// changeTextButton.addEventListener("click", function () {
+  // Змінюємо текст в текстовому елементі при натисканні на кнопку
+//   textElement.textContent = "Новий текст";
+// });
+
+// document.getElementById("get-started-btn").addEventListener("click", function () {
+//     document.getElementById("main-text").textContent = "Новий текст";
+// });
